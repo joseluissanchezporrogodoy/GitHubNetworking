478d2fd45fb3fca3485c0391d516a3931e62b067
José Luis Sánchez-Porro Godoy <joseluis.sanchezporro.godoy@gmail.com>
2024-12-05 11:32:35 +0100

    🚧 Add compatible platfotms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "GitHubNetworking",
+    platforms: [
+        .iOS(.v15),
+        .tvOS(.v15),
+        .macOS(.v15) // iPadOS se incluye bajo macCatalyst
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
